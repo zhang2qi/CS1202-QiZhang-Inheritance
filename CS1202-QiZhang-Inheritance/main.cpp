1af182601da0031ceda0a3ab6e9369e6c3c75f6e
Qi Zhang <jacky780203@sina.com>
2021-11-28 22:39:14 -0500

    Initial Commit

---
+++ CS1202-QiZhang-Inheritance/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS1202-QiZhang-Inheritance
+//
+//  Created by Qi Zhang on 11/28/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
